938dffc4c4ecaf359748cd1c5332b30f9b68362c
Joana Simões <joanasimoes04@gmail.com>
2022-10-30 08:52:56 +0000

    Add scale features func

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import minmax_scale
+from sklearn.preprocessing import StandardScaler
 
 def normalize_data(dataset, feature_range=(0, 1)):
     data = dataset.copy()
     data = minmax_scale(data, feature_range=feature_range, axis=0)
     return pd.DataFrame(data=data, columns=dataset.columns)
 
+def scale_features(dataset):
+    return (dataset-dataset.mean()) ( dataset.std())
 
 def append_arrays(data_array):
     data = data_array[0]
